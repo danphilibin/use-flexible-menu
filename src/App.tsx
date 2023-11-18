0a7c0855892e926edf6f79df00c46fe6f779f43a
Dan Philibin <dan@danphilibin.com>
2023-11-18 05:34:55 +0000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Menu, MenuButton, MenuItem, useMenuState } from "ariakit";
 import FlexibleMenu from "./FlexibleMenu";
+import classNames from "classnames";
 
 const foods = [
   "Pizza",
@@ -20,34 +21,51 @@ const foods = [
   "Curry",
 ];
 
-function App() {
+function WithTitle() {
   const [items, setItems] = useState(foods);
 
   const menu = useMenuState();
 
   return (
     <div className="text-left py-12 max-w-[50%] mx-auto">
-      <div className="bg-gray-50">
-        <FlexibleMenu
-          items={items}
-          itemRenderer={(item, idx) => (
-            <li
-              className="whitespace-no-wrap flex items-center py-2 px-3"
-              key={idx}
-            >
-              {item}
-            </li>
-          )}
-          moreButton={(overflowItems, moreRef) => (
-            <>
+      <div className="bg-gray-50 flex items-center">
+        <h1 className="flex-1 whitespace-nowrap">Test title</h1>
+        <div className="flex-0 flex justify-end">
+          <FlexibleMenu
+            height={40}
+            debug={false}
+            items={items}
+            itemRenderer={({ data, isVisible }, idx) => (
+              <li
+                className={classNames(
+                  "whitespace-nowrap flex items-center py-2 px-3",
+                  {
+                    "invisible absolute top-0 left-0 pointer-events-none":
+                      !isVisible,
+                  }
+                )}
+                key={idx}
+              >
+                {data}
+              </li>
+            )}
+            moreButtonRenderer={({ isVisible, moreRef }) => (
               <MenuButton
                 state={menu}
                 as="li"
-                className="whitespace-no-wrap flex items-center py-2 px-3"
+                className={classNames(
+                  "whitespace-no-wrap flex items-center py-2 px-3",
+                  {
+                    "absolute top-0 left-0 invisible pointer-events-none":
+                      !isVisible,
+                  }
+                )}
                 ref={moreRef}
               >
                 More &darr;
               </MenuButton>
+            )}
+            overflowRenderer={({ overflowItems }) => (
               <Menu state={menu} className="bg-white p-1 shadow-md rounded-md">
                 {overflowItems.map((item, idx) => (
                   <MenuItem
@@ -58,7 +76,88 @@ function App() {
                   </MenuItem>
                 ))}
               </Menu>
-            </>
+            )}
+          />
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <button
+          className="border px-2 mr-2 border-black"
+          onClick={() =>
+            setItems((prev) => [
+              ...prev,
+              foods[Math.floor(Math.random() * foods.length)],
+            ])
+          }
+        >
+          Add item
+        </button>
+        <button
+          className="border px-2 mr-2 border-black"
+          onClick={() => setItems((prev) => prev.slice(0, -1))}
+        >
+          Remove item
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function RightAligned() {
+  const [items, setItems] = useState(foods);
+
+  const menu = useMenuState();
+
+  return (
+    <div className="text-left py-12 max-w-[50%] mx-auto">
+      <div className="bg-gray-50">
+        <FlexibleMenu
+          height={40}
+          debug={false}
+          items={items}
+          direction="rtl"
+          itemRenderer={({ data, isVisible }, idx) => (
+            <li
+              className={classNames(
+                "whitespace-no-wrap flex items-center py-2 px-3",
+                {
+                  "invisible absolute top-0 left-0 pointer-events-none":
+                    !isVisible,
+                }
+              )}
+              key={idx}
+            >
+              {data}
+            </li>
+          )}
+          moreButtonRenderer={({ isVisible, moreRef }) => (
+            <MenuButton
+              state={menu}
+              as="li"
+              className={classNames(
+                "whitespace-no-wrap flex items-center py-2 px-3",
+                {
+                  "absolute top-0 left-0 invisible pointer-events-none":
+                    !isVisible,
+                }
+              )}
+              ref={moreRef}
+            >
+              More &darr;
+            </MenuButton>
+          )}
+          overflowRenderer={({ overflowItems }) => (
+            <Menu state={menu} className="bg-white p-1 shadow-md rounded-md">
+              {overflowItems.map((item, idx) => (
+                <MenuItem
+                  key={idx}
+                  className="block py-1.5 px-2 focus:bg-gray-50"
+                >
+                  {item}
+                </MenuItem>
+              ))}
+            </Menu>
           )}
         />
       </div>
@@ -82,6 +181,15 @@ function App() {
           Remove item
         </button>
       </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <WithTitle />
+      <RightAligned />
     </div>
   );
 }
--- src/FlexibleMenu.tsx
+++ src/FlexibleMenu.tsx
@@ -1,53 +1,150 @@
-import { CSSProperties } from "react";
-import useFlexibleMenu from "./useFlexibleMenu";
+import useResizeObserver from "@react-hook/resize-observer";
+import { useCallback, useEffect, useRef, useState } from "react";
 
-interface FlexibleMenuProps<T> {
-  items: T[];
-  itemRenderer: (item: T, index: number) => React.ReactNode;
-  debug?: boolean;
-  menuClassName?: string;
-  moreButton?: (
-    overflowItems: T[],
-    moreRef?: ReturnType<typeof useFlexibleMenu>["moreRef"]
-  ) => React.ReactNode;
+function getFittingItems(
+  container: HTMLUListElement,
+  moreItemWidth: number
+): number {
+  const spaceTarget = Math.ceil(container.offsetWidth);
+
+  const children = Array.from(container.children).filter(
+    // TODO: better detection of More button?
+    (child) => child.getAttribute("role") !== "button"
+  );
+  const sizes = children.map((li) => li.getBoundingClientRect());
+
+  let spaceUsed = 0;
+
+  for (let i = 0; i < sizes.length; i++) {
+    const itemWidth = Math.ceil(sizes[i].width);
+
+    if (spaceUsed + itemWidth > spaceTarget) {
+      // the current item does not fit; start with the previous item
+      let slicePosition = Math.max(0, i - 1);
+
+      while (spaceUsed + moreItemWidth > spaceTarget) {
+        spaceUsed -= Math.ceil(sizes[slicePosition].width);
+        slicePosition--;
+        if (slicePosition < 0) break;
+      }
+
+      return slicePosition;
+    }
+
+    spaceUsed += itemWidth;
+  }
+
+  return -1;
 }
 
-// type Visibility = "visible" | "ghost";
+function getIsVisible(slicePosition: number | undefined, index: number) {
+  // not calculated yet
+  if (slicePosition === undefined) return false;
+
+  // all items are visible
+  if (slicePosition === -1) return true;
+
+  return slicePosition >= index;
+}
+
+interface FlexibleMenuState {
+  itemHeight: number;
+  moreWidth: number;
+  slicePosition: number | undefined;
+}
+
+function useFlexibleMenu({ items }: { items: any[] }) {
+  const [state, setState] = useState<FlexibleMenuState>({
+    slicePosition: undefined,
+    itemHeight: 0,
+    moreWidth: 0,
+  });
+
+  const navRef = useRef<HTMLUListElement>(null);
+  const moreRef = useRef<HTMLLIElement>(null);
+
+  const calculate = useCallback(() => {
+    if (!navRef.current) return;
+
+    const moreWidth = Math.ceil(
+      moreRef.current?.getBoundingClientRect().width ?? 0
+    );
+
+    const slicePosition = getFittingItems(navRef.current, moreWidth);
+
+    const firstItem = navRef.current.children[0];
+    const firstItemHeight = firstItem
+      ? Math.ceil(firstItem.getBoundingClientRect().height)
+      : 0;
+
+    setState({
+      itemHeight: firstItemHeight,
+      slicePosition,
+      moreWidth,
+    });
+  }, []);
+
+  useResizeObserver(navRef, calculate);
+
+  useEffect(calculate, [items, calculate]);
+
+  const overflowItems =
+    state.slicePosition === undefined || state.slicePosition === -1
+      ? []
+      : items.slice(state.slicePosition + 1);
+
+  return {
+    navRef,
+    moreRef,
+    overflowItems,
+    height: state.itemHeight,
+    slicePosition: state.slicePosition,
+  };
+}
+
+interface FlexibleMenuProps<T> {
+  // required; prevents individual items from resizing the parent container
+  // and triggering an infinite ResizeObserver loop
+  height: number;
+  items: T[];
+  itemRenderer: (
+    item: {
+      data: T;
+      isVisible: boolean;
+    },
+    index: number
+  ) => React.ReactNode;
+  debug?: boolean;
+  menuClassName?: string;
+  direction?: "ltr" | "rtl";
+  moreButtonRenderer: (args: {
+    moreRef?: ReturnType<typeof useFlexibleMenu>["moreRef"];
+    isVisible: boolean;
+  }) => React.ReactNode;
+  overflowRenderer: (args: { overflowItems: T[] }) => React.ReactNode;
+}
+
+// TODO: does not work with flexible-width containers, because
+// every time we hide items the container resizes and ResizeObserver recalculates.
 
 export default function FlexibleMenu<T>(props: FlexibleMenuProps<T>) {
   const {
+    height,
     items,
     itemRenderer,
+    direction = "ltr",
     debug = true,
     menuClassName = "",
-    moreButton = () => null,
+    moreButtonRenderer,
   } = props;
 
-  const { visibleItems, overflowItems, navRef, moreRef, height } =
-    useFlexibleMenu({
-      items,
-    });
-
-  // const menuStyles: Record<Visibility, CSSProperties> = {
-  //   visible: {
-  //     height,
-  //   },
-  //   // ghost: {
-  //   //   position: "absolute",
-  //   //   visibility: "hidden",
-  //   //   top: 0,
-  //   //   left: 0,
-  //   // },
-  // };
-
-  // const vis: Visibility[] = ["visible", "ghost"];
-
-  console.log("items", items);
+  const { overflowItems, navRef, moreRef, slicePosition } = useFlexibleMenu({
+    items,
+  });
 
   return (
-    <div style={{ position: "relative" }}>
+    <>
       <ul
-        // ref={v === "ghost" ? navRef : undefined}
         ref={navRef}
         style={{
           display: "flex",
@@ -55,26 +152,45 @@ export default function FlexibleMenu<T>(props: FlexibleMenuProps<T>) {
           flexWrap: "wrap",
           overflow: "hidden",
           width: "100%",
+          justifyContent: direction === "ltr" ? "start" : "end",
+          flexDirection: direction === "ltr" ? "row" : "row-reverse",
+          // height is important; prevents us from ever seeing a second row
           height,
-          // ...menuStyles[v],
+          visibility: slicePosition === undefined ? "hidden" : "visible",
         }}
         className={menuClassName}
       >
-        {items.map(itemRenderer)}
-        {moreButton(overflowItems, moreRef)}
+        {items.map((data, idx) =>
+          itemRenderer(
+            {
+              data,
+              isVisible: getIsVisible(slicePosition, idx),
+            },
+            idx
+          )
+        )}
+        {moreButtonRenderer({
+          isVisible:
+            slicePosition !== undefined &&
+            slicePosition !== -1 &&
+            items.length > slicePosition + 1,
+          moreRef,
+        })}
       </ul>
+
+      {overflowItems.length > 0 && props.overflowRenderer({ overflowItems })}
 
       {debug && (
         <div className="mt-8 font-mono text-left text-sm">
           <p>Total items: {items.length}</p>
-          <p>Visible: {visibleItems.length}</p>
           <p>Hidden: {overflowItems.length}</p>
+          <p>slicePosition: {slicePosition}</p>
           <p className="mt-4">
             Hidden items:{` `}
             {overflowItems.length > 0 ? overflowItems.join(", ") : "none"}
           </p>
         </div>
       )}
-    </div>
+    </>
   );
 }
